ead1afd6a2624d56aabb8eb76f0b7a3e6c372ef8
omar.ezz <omarezz332@gmail.com>
2023-02-20 00:02:24 +0200

    implement methods in FakeDataSource.kt

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,24 +4,53 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminderList: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
+    private var returnError = false
+    fun setReturnError(returnErr: Boolean) {
+        returnError = returnErr
+    }
 
-//    TODO: Create a fake data source to act as a double to the real data source
-
+    //    TODO: Create a fake data source to act as a double to the real data source
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (returnError) {
+            return Result.Error("Error getting reminders")
+        } else {
+            return Result.Success(ArrayList(reminderList!!))
+        }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminderList?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (returnError) {
+            return Result.Error("Error getting reminders")
+        } else {
+            val reminder = reminderList?.find {
+                it.id == id
+            }
+            return if (reminder != null) {
+                Result.Success(reminder)
+
+            } else {
+
+                Result.Error("reminders were unable to get retrieved")
+            }
+        }
+
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminderList?.clear()
+    }
+
+    override suspend fun delete(id: String) {
+        reminderList?.remove(reminderList?.find {
+            it.id == id
+        })
+
     }
 
 
